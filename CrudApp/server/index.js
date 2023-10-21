c064c459f4aa96260b4668a2588faa96b04b5f11
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-10-21 19:13:12 +0530

    Create index.js

---
+++ CrudApp/server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import cors from 'cors';
+import http from 'http';
+import {typeDefs, resolvers} from './Graphql/users.js';
+import db from './database.js';
+
+const app = express();
+app.use(express.json({ limit: '15gb' }));
+app.use(cors());
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+app.use(
+  '/graphql',
+  expressMiddleware(server, {
+    context: async ({ req }) => ({DB:db}),
+  }),
+);
+
+new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
