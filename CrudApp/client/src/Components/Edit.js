510c808722058c89406338c226382330230ba018
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-11-02 19:23:22 +0530

    Update Edit.js

--- CrudApp/client/src/Components/Edit.js
+++ CrudApp/client/src/Components/Edit.js
@@ -2,70 +2,98 @@ import React, { Fragment, useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker';
 import { useMutation, useQuery } from '@apollo/client';
 import { EDIT_USER, VIEW_USER } from '../Queries';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, ProgressBar } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
-import { toDateStr, fileToBase64 } from '../Convert';
+import { toDateStr } from '../Convert';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function  Edit() {
     let history = useNavigate();
-
+    const [files, setFiles] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [fr,] = useState(new FileReader());
     const [uid, setUId] = useState(0);
     const [name, setName] = useState('');  
     const [email, setEmail] = useState(''); 
     const [jobTitle, setJobTitle] = useState(''); 
     const [joiningDate, setJoiningDate] = useState('');  
     const [content, setContent] = useState(''); 
-    const [mime, setMime]=useState('');
-
+    const [initialise, setInitialise] =  useState(false);
     useEffect(() => {
-        setUId(parseInt(localStorage.getItem('id')))//set the integer value
-    },[]);
+        if(uid===0)
+            setUId(parseInt(localStorage.getItem('id')))//set the integer value
+    },[uid]);
     const {data, loading, error} = useQuery(VIEW_USER, {variables:{id:uid}});
-    useEffect(() => {
+    const [initialiseData = () => {
         if(data && data.user){
             setName(data.user.name)
             setEmail(data.user.email)
             setJobTitle(data.user.job_title)
             setJoiningDate(toDateStr(new Date(data.user.joining_date)))
             setContent(data.user.content) //content = base64 string
-            setMime(data.user.mime)
+            setInitialise(true);
         }
-    },[data]); 
- 
+    },] = useState();
+
+    useEffect(() => {
+        if(!initialise){
+            initialiseData();
+        }
+    },[initialise, initialiseData]);
+
+    const [loadPromise = async ()=> {
+        return new Promise((resolve, reject) => { 
+            fr.onerror = (err) => reject(err);  
+            fr.onload = () => resolve(fr.result);
+            fr.onprogress = (event) => {
+                setTimeout(()=>{
+                    var pr = Math.round(100*(event.loaded / event.total));
+                    if (pr > progress && pr <= 100) {setProgress(pr);}            
+                },1);
+            }      
+        });
+    },] = useState();
+
+    useEffect(() => {
+        (async () => {
+            if(files && files[0]) {
+                var result = await loadPromise();
+                setContent(result);
+            }
+        })();
+    },[files, loadPromise]); 
+
     const [changeUser] = useMutation(EDIT_USER,)
     
     if(loading) return <Fragment>loading...</Fragment>
     if(error) return <Fragment>error...</Fragment>
     //refetch();//refetch the query when redirecting
-        
-    const changeContent = (e) => {
-        if (e.target.files[0]) {
-            const file = e.target.files[0];
-            setMime('image/'+file.name.split('.')[1]);
-            fileToBase64(file, function(base64Data){
-                console.log(base64Data);
-                setContent(base64Data.split(',')[1]);
-            })
-        }
-    }
 
+    async function handleChange (e) {
+        if(e.target && e.target.files) {
+            setFiles(e.target.files);
+            setProgress(0);
+            fr.readAsDataURL(e.target.files[0]);
+        }
+    } 
+    
     const handelSubmit = async (e) => {
         e.preventDefault();  // Prevent reload
-        let b=name, c=email, d=jobTitle, f=joiningDate, 
-        g = content, h=mime;  //sent to server
+        let b=name, c=email, d=jobTitle, f=joiningDate, g = content; 
+        //sent to server
         try{
             await changeUser({variables:{id:uid, name:b, email:c, job_title:d, 
-                joining_date:f, content:g, mime:h}})
+                joining_date:f, content:g}})
             history('/') //redirect to home
         }catch(error){alert("error in edit: "+error);}
     }
+    
     return (
         <div>
-            <Form className="d-grid gap-2" style={{marginLeft:'25rem', 
-                        marginRight:'25em', marginTop:'2rem'}}>
+            <Form className="d-grid gap-2" style={{marginLeft: '25em', 
+                    marginRight: '25em', marginTop: '2em'}}>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Control  value={name}
                         onChange={e => setName(e.target.value)}
@@ -89,10 +117,10 @@ function  Edit() {
                 </div>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPhoto">
-                    <Form.Control onChange={async(e) => 
-                    {if(e.target && e.target.files) changeContent(e)}}
+                    <Form.Control onChange={async(e) => await handleChange(e)}
                     type="file" />
                 </Form.Group>
+                <ProgressBar now={progress} label={`${progress}%`} id="pb"></ProgressBar>
                 <div>
                     <Link  to='/'>
                         <Button variant="primary" size="lg">
@@ -104,8 +132,7 @@ function  Edit() {
                         }variant="warning" type="submit" size="lg">
                         Update
                     </Button>
-                    <img src={'data:'+mime+';base64,'+content} 
-                        width={75} height={75} alt='' />
+                    <img src={content} width={75} height={75} alt='' />
                 </div>
             </Form>
         </div>
