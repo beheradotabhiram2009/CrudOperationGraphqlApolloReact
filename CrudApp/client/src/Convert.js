617fc3d3f388f07f8e10dfe4ebe957e86355d17e
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-11-05 07:36:29 +0530

    Update Convert.js

--- CrudApp/client/src/Convert.js
+++ CrudApp/client/src/Convert.js
@@ -1,15 +1,27 @@
-//file to base64
-export const fileToBase64 =(file, callback) =>{
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.onerror = (error) => {
-        alert('Load Error: '+ error);
-    };
-    reader.readAsDataURL(file);
-};
-
 //to set the date
-export const toDateStr=(dt)=>{
+export const toDateStr = (dt) =>{
     const m = dt.getMonth()+1;
     return (dt.getFullYear() + '-' + m + '-' + dt.getDate());
+};
+
+export const readFile = async(fr, progress, callback) => {
+    return new Promise((resolve, reject) => {
+        fr.onload = () => {
+            callback(100);
+            resolve(fr.result);
+        };
+        fr.onerror = (err) => {
+            callback(0);
+            reject(err);
+        };
+        fr.onprogress = (event) => {
+            if (!event.lengthComputable) return;
+            setTimeout(()=>{
+                var pr = Math.round(100*(event.loaded / event.total));
+                if (pr > progress && pr <= 100) callback(pr); 
+                else if (pr <= progress) callback(progress);
+                else callback(100);
+            },1);
+        };
+    })
 };
